c997a0aa250981c14fa5ea4859f69619aa76f09f
James Chen <james.chen@iam-javelin.com>
2017-12-11 13:49:13 +1300

    added some flags

--- src/PlSql.kt
+++ src/PlSql.kt
@@ -5,10 +5,12 @@ import org.antlr.v4.runtime.tree.ParseTreeWalker
 import org.json.JSONObject
 import java.io.ByteArrayInputStream
 import java.io.File
+import java.nio.charset.Charset
 
 class PlSql: PlSqlParserBaseListener() {
 	val document by lazy { Document() }
 	var tokenList = listOf<Token>()
+	var code = ""
 
 	override fun exitMain(ctx: PlSqlParser.MainContext?) {
 		document.parse(ctx!!)
@@ -16,7 +18,8 @@ class PlSql: PlSqlParserBaseListener() {
 
 	fun parse(fileName: String): PlSql {
 		val file = File(fileName)
-		val stream = ByteArrayInputStream(file.readBytes())
+		val bytes = file.readBytes()
+		val stream = ByteArrayInputStream(bytes)
 		val lexer = PlSqlLexer(CharStreams.fromStream(stream))
 		val tokens = CommonTokenStream(lexer)
 		val parser = PlSqlParser(tokens)
@@ -24,34 +27,80 @@ class PlSql: PlSqlParserBaseListener() {
 
 		document.setFile(file.name)
 		walker.walk(this, parser.javln())
-		lexer.reset()
-		tokenList = lexer.allTokens
+
+		if (Flags.t) {
+			lexer.reset()
+			tokenList = lexer.allTokens
+		}
+
+		if (Flags.c) {
+			code = bytes.toString(Charset.forName("UTF-8"))
+		}
+
 		return this
 	}
 
 	companion object {
-		private fun tokenToJson(token: Token) = """{
-			"r":${token.line},
-			"c":${token.charPositionInLine},
-			"b":${token.startIndex},
-			"e":${token.stopIndex+1},
-			"t":${token.type}
-		}""".replace(Regex("\\n\\s*"), "")
+		data class TokenData(
+			val r: Int,	// row
+			val c: Int,	// col
+			val b: Int,	// beg
+			val e: Int,	// end
+			val t: Int	// type
+		)
 
-		private val debugFileNames by lazy { debugFiles.map { "$debugPath$it" } }
-		private val debugPath by lazy { "./javln/" }
-		private val debugFiles by lazy { arrayOf(
-			"procedure_claim_change_cpay_prop_reco.sql"
-//		private val debugFileName by lazy { "procedure_policy_unposted_policy_trans.sql" }
-		) }
+		private fun createTokenData(token: Token) = TokenData(
+			token.line, token.charPositionInLine, token.startIndex, token.stopIndex+1, token.type
+		)
+
+		private var files = mutableListOf<String>()
+
+		object Flags {
+			var t = false // token
+			var c = false // code
+			var h = false // help
+		}
+
+		private val helpMessage = """
+    |Javelin PL/SQL Parser Usage
+    |	-h	Help:	Display this message
+    |	-t	Tokens:	Generate JSON with Tokens
+    |	-c	Code:	Generate JSON with Code
+		""".trimMargin()
+
 
 		@JvmStatic
 		fun main(args: Array<String>) {
-			val files = if (args.size > 0) args.toList() else debugFileNames
-			val docs = files.map { PlSql().parse(it).document }
+			args.forEach {
+				if (it.startsWith('-')) {
+					when (it[1]) {
+						't' -> Flags.t = true
+						'c' -> Flags.c = true
+						'h' -> Flags.h = true
+					}
+				} else {
+					files.add(it)
+				}
+			}
+
+			if (Flags.h || files.isEmpty()) {
+				println(helpMessage)
+				return
+			}
+
+			val docs = files.map { PlSql().parse(it) }
 			val jsonMap = mutableMapOf<String, Any?>(
 				"symbol_types" to SynItem.Global.symbolTypes,
-				"files" to docs.map { it.toDataMap() }
+				"files" to docs.map {
+					val r = it.document.toDataMap()
+					if (Flags.t) {
+						r["tokens"] = it.tokenList.map { createTokenData(it) }
+					}
+					if (Flags.c) {
+						r["code"] = it.code
+					}
+					r
+				}
 			)
 			println(JSONObject(jsonMap))
 		}
